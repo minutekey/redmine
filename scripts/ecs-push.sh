5ba93c72eea3f3c58c91a66c93c391f64c729f86
Shepherd McIlroy <github@duxfu.net>
2025-05-08 18:02:11 -0600

    LIN-3964 Codebuild scripts

---
+++ scripts/ecs-push.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+usage()
+{
+    cat <<EOF
+Usage: $0 taskName workspace version(default: latest)
+EOF
+    return 0
+}
+
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+account_id=$(aws sts get-caller-identity --query Account --output text)
+if [ $? -ne 0 ]; then
+    echo "Assume a role before running this script"
+    exit 4
+fi
+
+taskName=${1}
+workspace=${2}
+version=${3:-latest}
+
+if [[ -z "$taskName" ]]; then
+    echo "Provide a task name"
+    usage
+    exit 1
+fi
+
+if [[ -z "$workspace" ]]; then 
+    echo "Provide a workspace name"
+    usage
+    exit 1
+fi
+
+gitHash=$(git log -n 1 --format=%h --abbrev=7)
+gitBranch=$(git rev-parse --abbrev-ref HEAD)
+gitTag=$(git tag --points-at HEAD)
+branch=${gitBranch//\//_}
+tag=${gitTag//\//_}
+if [[ -z "$tag" ]]; then tagOrBranch=$branch; else tagOrBranch=$tag; fi
+if [[ $tagOrBranch == *"release_"* ]]; then isRelease=true; else isRelease=false; fi
+
+if [[ ($version == latest) && ($isRelease == true) ]]; then 
+    imageTag=${tagOrBranch}-${gitHash}
+else 
+    imageTag=${version}-${tagOrBranch}-${gitHash}
+fi
+
+region=${AWS_REGION-$(aws configure get region)}
+image="$account_id.dkr.ecr.$region.amazonaws.com/$workspace/$taskName:$imageTag"
+
+
+if docker inspect $image >/dev/null 2>&1; then
+    docker rmi $image
+fi
+
+set -e
+trap 'echo -e "\n\033[0;31mFailed to push image to ecr / update $workspace-$taskName-service\033[0m"; exit 1' ERR
+
+aws ecr get-login-password --region $region | docker login --username AWS --password-stdin $account_id.dkr.ecr.$region.amazonaws.com
+docker tag $taskName:latest $image
+docker push $image
+
+TASK_DEFINITION=$(aws ecs describe-task-definition --task-definition "${workspace}-$taskName" --region "$region")
+NEW_TASK_DEFINITION=$(echo $TASK_DEFINITION | jq --arg IMAGE "$image" '.taskDefinition | .containerDefinitions[0].image = $IMAGE | del(.taskDefinitionArn) | del(.revision) | del(.status) | del(.requiresAttributes) | del(.compatibilities) |  del(.registeredAt)  | del(.registeredBy)')
+NEW_TASK_INFO=$(aws ecs register-task-definition --region "$region" --cli-input-json "$NEW_TASK_DEFINITION")
+NEW_REVISION=$(echo $NEW_TASK_INFO | jq '.taskDefinition.revision')
+aws ecs update-service --cluster default-fargate --service $workspace-$taskName-service --task-definition ${workspace}-$taskName:${NEW_REVISION} >/dev/null 2>&1
+echo -e "\033[0;32mSuccessfully updated $workspace-$taskName-service\033[0m"
