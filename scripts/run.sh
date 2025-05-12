d49a762afe100ee563125173d01c9626c6944a99
Shepherd McIlroy <github@duxfu.net>
2025-05-12 17:42:06 -0600

    LIN-3965 Add s3 plugin

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,6 +4,24 @@ set -e
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$script_dir"/..
 
+if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
+    echo "Warning: AWS_SECRET_ACCESS_KEY not set, S3 attachment storage disabled"
+
+elif [ -z "$WORKSPACE" ]; then
+    echo "Warning: WORKSPACE not set, S3 attachment storage disabled"
+
+else
+    cat > "$script_dir/../config/s3.yml" << EOF
+development:
+  access_key_id: "${AWS_ACCESS_KEY_ID}"
+  secret_access_key: "${AWS_SECRET_ACCESS_KEY}"
+  session_token: "${AWS_SESSION_TOKEN}"
+  bucket: "$WORKSPACE-rds-redmine-files"
+  folder: ""
+  region: "${AWS_REGION}"
+EOF
+fi
+
 export REDMINE_NO_DB_MIGRATE=true
 export RAILS_ENV=development
 
