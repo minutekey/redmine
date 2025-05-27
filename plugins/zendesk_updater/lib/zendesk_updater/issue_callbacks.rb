96c74508a0eb7aa9260afe866d98b0e06e69c902
Shepherd McIlroy <github@duxfu.net>
2025-05-26 22:06:01 -0600

    Debug

--- plugins/zendesk_updater/lib/zendesk_updater/issue_callbacks.rb
+++ plugins/zendesk_updater/lib/zendesk_updater/issue_callbacks.rb
@@ -9,7 +9,15 @@ module ZendeskUpdater
     private
 
     def trigger_lambda_on_issue_update
-      LambdaClient.invoke_lambda(self)
+      Rails.logger.info "ZendeskUpdater: Attempting to invoke lambda for issue #{id}"
+      
+      begin
+        LambdaClient.invoke_lambda(self)
+        Rails.logger.info "ZendeskUpdater: Lambda invocation completed for issue #{id}"
+      rescue => e
+        Rails.logger.error "ZendeskUpdater: Lambda invocation failed for issue #{id}: #{e.message}"
+        Rails.logger.error e.backtrace.join("\n")
+      end
     end
   end
 end
