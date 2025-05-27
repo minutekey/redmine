76c118496384ec14432f1d097a18fbb3e0ba4b10
Shepherd McIlroy <github@duxfu.net>
2025-05-27 10:42:00 -0600

    debug

--- plugins/zendesk_updater/init.rb
+++ plugins/zendesk_updater/init.rb
@@ -11,18 +11,53 @@ Redmine::Plugin.register :zendesk_updater do
   requires_redmine version_or_higher: '4.0.0'
 end
 
+puts "=== LOADING ZENDESK UPDATER MODULES ==="
+STDOUT.flush
+
 require_relative 'lib/zendesk_updater/issue_callbacks'
 require_relative 'lib/zendesk_updater/journal_callbacks'
 require_relative 'lib/zendesk_updater/lambda_client'
 
+puts "=== MODULES LOADED, CHECKING MODEL CLASSES ==="
+puts "Issue class exists: #{defined?(Issue)}"
+puts "Journal class exists: #{defined?(Journal)}"
+STDOUT.flush
+
 Rails.application.config.to_prepare do
-  unless Issue.included_modules.include?(ZendeskUpdater::IssueCallbacks)
-    Issue.include(ZendeskUpdater::IssueCallbacks)
+  puts "=== TO_PREPARE BLOCK EXECUTING ==="
+  
+  if defined?(Issue)
+    unless Issue.included_modules.include?(ZendeskUpdater::IssueCallbacks)
+      puts "Including IssueCallbacks in Issue model"
+      Issue.include(ZendeskUpdater::IssueCallbacks)
+    else
+      puts "IssueCallbacks already included in Issue model"
+    end
+    
+    # Check if callbacks are registered
+    issue_callbacks = Issue._commit_callbacks.select { |cb| cb.filter.to_s.include?('lambda') }
+    puts "Issue commit callbacks with 'lambda': #{issue_callbacks.size}"
+  else
+    puts "Issue class not yet defined"
   end
   
-  unless Journal.included_modules.include?(ZendeskUpdater::JournalCallbacks)
-    Journal.include(ZendeskUpdater::JournalCallbacks)
+  if defined?(Journal)
+    unless Journal.included_modules.include?(ZendeskUpdater::JournalCallbacks)
+      puts "Including JournalCallbacks in Journal model"
+      Journal.include(ZendeskUpdater::JournalCallbacks)
+    else
+      puts "JournalCallbacks already included in Journal model"
+    end
+    
+    # Check if callbacks are registered
+    journal_callbacks = Journal._commit_callbacks.select { |cb| cb.filter.to_s.include?('lambda') }
+    puts "Journal commit callbacks with 'lambda': #{journal_callbacks.size}"
+  else
+    puts "Journal class not yet defined"
   end
+  
+  STDOUT.flush
+  puts "=== TO_PREPARE BLOCK COMPLETE ==="
 end
 
 puts "=== ZENDESK UPDATER PLUGIN LOADED ==="
