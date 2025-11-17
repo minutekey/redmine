30e7aacb9c9a95e9cf77e2ae00d22acdbc85dae6
Shepherd McIlroy <github@duxfu.net>
2025-11-17 14:16:10 -0700

    LIN-4423 Fix logout link

--- lib/redmine/meru_jwt_auth.rb
+++ lib/redmine/meru_jwt_auth.rb
@@ -26,6 +26,15 @@ module Redmine
         "https://#{host}/#/"
       end
 
+<<<<<<< Updated upstream
+=======
+      # Get the Meru logout URL
+      def meru_logout_url
+        host = ENV['MERU_HOST'] || "#{ENV['WORKSPACE']}.meru.dev.minutekey.com"
+        "https://#{host}/logout"
+      end
+
+>>>>>>> Stashed changes
       # Get the AWS region for SSM
       def aws_region
         ENV['AWS_REGION'] || 'us-east-1'
