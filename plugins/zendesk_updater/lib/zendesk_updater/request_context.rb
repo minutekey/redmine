931a57f22400a5a5ec3cbf57e7e70952c8b5f66d
Will Freeman <hohosanta@me.com>
2025-12-04 14:50:08 -0700

    only ignore parent issue when sending updates to zendesk from pokemon-api

--- plugins/zendesk_updater/lib/zendesk_updater/request_context.rb
+++ plugins/zendesk_updater/lib/zendesk_updater/request_context.rb
@@ -4,10 +4,11 @@ module ZendeskUpdater
   # Store request-scoped data using ActiveSupport::CurrentAttributes
   # This makes data available throughout the request lifecycle, including in model callbacks
   class RequestContext < ActiveSupport::CurrentAttributes
-    attribute :zendesk_ignore
+    attribute :zendesk_ignore_issue_id
 
     def self.set_from_headers(headers)
-      self.zendesk_ignore = headers['X-Zendesk-Ignore'].to_s.downcase == 'true'
+      ignore_header = headers['X-Zendesk-Ignore'].to_s.strip
+      self.zendesk_ignore_issue_id = ignore_header.present? ? ignore_header.to_i : nil
     end
   end
 end
